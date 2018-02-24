e1f5b45aaac54d49afce8f2993140d3004513931
Kireet Agrawal <kireet@berkeley.edu>
2018-02-24 12:34:35 -0800

    added thermocoupling

--- HAB_Sys/HAB_Sys.ino
+++ HAB_Sys/HAB_Sys.ino
@@ -10,7 +10,7 @@
 
  TODO:
  -Set up Real Time Clock for everything to use.
- -Set up Tracksoar
+ -Set up Tracksoar & 2ndary
  -Set up Altimeter
  -Actuation Code w/ conditions
  -Wire diagram
@@ -23,20 +23,38 @@
 #include <SD.h>
 #include <Wire.h> // I2C library
 #include "Adafruit_SI1145.h"  // UV sensor library
+#include "Adafruit_MAX31855.h"  // Thermocouple library
 #include <Servo.h>
+
+// Arduino Mega SPI Pins
+#define MISO 50
+#define MOSI 51
+#define CLK 52
+
+// Arduino Mega I2C Pins
+#define SCL 21
+#define SDA 20
 
 
 File sd_card_file; // SD card file
+int sd_card_pin = 53;
+String curr_data = "";
+String delimiter = "/~/";  // Data string delimiter for SD logging b/w sensors
+String line_delimiter = "\\\r";  // Newline delimiter for SD logging
+
 Adafruit_SI1145 uv_sensor = Adafruit_SI1145();  // uv sensor object declaration
 
 Servo servo1;
 Servo servo2;
-int servo1extended = false;
-int servo2extended = false;
-int servo1locked = false;
-int servo2locked = false;
+int servo1_pin = 9;
+int servo2_pin = 10;
+int servo1_extended = false;
+int servo2_extended = false;
 
-String delimiter = ",";  // Data string delimiter for SD logging 
+// Thermo Couple
+int thermoCS = 48;
+Adafruit_MAX31855 thermocouple(CLK, thermoCS, MISO);  // Initializes the Thermocouple
+
 
 
 void setup() {
@@ -45,15 +63,45 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+  setup_SD();
+  setup_UV();
+  setup_thermo();
+  
+  setup_servos();  // Experiment specific linear actuator setup
+  
+}
 
-  // Set up SD card
-  Serial.print("Initializing SD card...");
-  if (!SD.begin(53)) {
-    Serial.println("SD initialization failed!");
-    return;
-  }
-  Serial.println("SD initialization done.");
+void loop() {
+  // put your main code here, to run repeatedly:
+  String uv_data = get_uv_data();
+  Serial.println(uv_data);
+  curr_data += uv_data;
+  curr_data += delimiter;
 
+  String thermo_data = get_thermo_data();
+  Serial.println(thermo_data);
+  curr_data += thermo_data;
+  curr_data += delimiter;
+
+  curr_data += line_delimiter;
+  write_to_sd("test.txt", curr_data);
+
+  
+  // 90-100K feet altitude
+  // Timer 900 seconds
+
+  // 35-45K feet altitude
+  // Timer 900 seconds
+
+  curr_data = "";
+  delay(1000);
+}
+
+
+/*
+ * UV sensor Specific
+ */
+void setup_UV() {
   // Set up UV Sensor (SI1145)
   Serial.print("Initializing UV Sensor...");
   if (! uv_sensor.begin()) {
@@ -61,22 +109,7 @@ void setup() {
     return;
   }
   Serial.println("UV Sensor initialization done.");
-
-  Serial.print("Initializiation Servos...");
-  servo1.attach(9);
-  servo2.attach(10);
-  Serial.println("Servo initialization done");
-
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  String uv_data = get_uv_data();
-  Serial.println(uv_data);
-  write_to_sd("test.txt", uv_data);
-  delay(1000);
-}
-
 String get_uv_data() {
   String uv_data = "";
   uv_data += String(uv_sensor.readUV());
@@ -87,13 +120,45 @@ String get_uv_data() {
   return uv_data;
 }
 
+/*
+ * Thermo Couple Sensor Setup
+ */
+void setup_thermo() {
+  // Thermo couple is setup when creating the object.
+  Serial.println("Initializing Thermo Couple...");
+  delay(500);  // wait for MAX thermo chip to stabilize
+}
+String get_thermo_data() {
+  String thermo_data = "";
+  thermo_data += thermocouple.readInternal();
+  thermo_data += delimiter;
+  double c = thermocouple.readCelsius();
+   if (isnan(c)) {
+     Serial.println("Something wrong with thermocouple!");
+   } else {
+     thermo_data += String(c);
+   }
+   return thermo_data;
+}
+
+/*
+ * SD Card Specific
+ */
+void setup_SD() {
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(sd_card_pin)) {
+    Serial.println("SD initialization failed!");
+    return;
+  }
+  Serial.println("SD initialization done.");
+}
 void write_to_sd(String filename, String data) {
   // The following is the code to write to sd card
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
   sd_card_file = SD.open(filename, FILE_WRITE);
 
-  // if the file opened okay, write to it:
+  // if the file opened, write to it:
   if (sd_card_file) {
     Serial.print("Writing to file ...");
     sd_card_file.println(data);
@@ -106,46 +171,28 @@ void write_to_sd(String filename, String data) {
   }
 }
 
-void run_servo1() {
-  Serial.println("Here");
-  
-  
-  delay(15000);
-  uv_data = get_uv_data();
-  Serial.println(uv_data);
-  write_to_sd("test.txt", uv_data);
-  delay(1000);
-  delay(15000);
-  Serial.println("Now Here");
-  
+/*
+ * Experiment's Actuator Specific
+ */
+void setup_servos() {
+  Serial.print("Initializiation Servos...");
+  servo1.attach(servo1_pin);
+  servo2.attach(servo2_pin);
+  Serial.println("Servo initialization done");
 }
-
-void extend_servo1() {
-  servo1.write(165);
+void extend_servo(int servo_id) {
+  if (servo_id == 1) {
+    servo1.write(165);
+  } elif (servo_id == 2) {
+    servo2.write(165);
+  }
 }
-
-void lock_servo1() {
-// 35-45K feet altitude
-// Timer 900 seconds
-  servo1.write(0);
-}
-
-void return_servo1() {
-  servo1.write(45);
-}
-
-void extend_servo2() {
-  servo2.write(165);
-}
-
-void lock_servo2() {
-// 90-100K feet altitude
-// Timer 900 seconds
-  servo2.write(0);
-}
-
-void return_servo2() {
-  servo2.write(45);
+void return_servo(int servo_id) {
+  if (servo_id == 1) {
+    servo1.write(45);
+  } elif (servo_id == 2) {
+    servo2.write(45);
+  }
 }
 
 
