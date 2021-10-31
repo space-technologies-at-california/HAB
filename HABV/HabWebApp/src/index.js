ac047704b0075eecfaa928757d364d806d1be979
Raymond Mo <raymondmo@berkeley.com>
2021-10-31 13:46:39 -0700

    Attempted to add overlay feature

--- HABV/HabWebApp/src/index.js
+++ HABV/HabWebApp/src/index.js
@@ -1,5 +1,8 @@
+import Feature from 'ol/Feature';
 import Map from 'ol/Map';
 import View from 'ol/View';
+import Overlay from 'ol/Overlay';
+
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 import { fromLonLat } from 'ol/proj';
@@ -17,7 +20,61 @@ const map = new Map({
    })
  ],
  view: new View({
-   center: fromLonLat([-122.2730, 37.8715]),
+   center: startPoint,
    zoom: 12
  })
+});
+
+//Data popup code
+const element = document.getElementById('popup');
+const popup = new Overlay({
+  element: element,
+  positioning: 'bottom-center',
+  stopEvent: false,
+  offset: [0, -10],
+});
+
+map.addOverlay(popup);
+
+function formatCoordinate(coordinate) {
+  return `
+    <table>
+      <tbody>
+        <tr><th>lon</th><td>${coordinate[0].toFixed(2)}</td></tr>
+        <tr><th>lat</th><td>${coordinate[1].toFixed(2)}</td></tr>
+      </tbody>
+    </table>`;
+}
+
+const info = document.getElementById('info');
+map.on('moveend', function () {
+  const view = map.getView();
+  const center = view.getCenter();
+  info.innerHTML = formatCoordinate(center);
+});
+
+map.on('click', function (event) {
+  const feature = map.getFeaturesAtPixel(event.pixel)[0];
+  if (feature) {
+    const coordinate = feature.getGeometry().getCoordinates();
+    popup.setPosition(coordinate);
+    $(element).popover({
+      container: element.parentElement,
+      html: true,
+      sanitize: false,
+      content: formatCoordinate(coordinate),
+      placement: 'top',
+    });
+    $(element).popover('show');
+  } else {
+    $(element).popover('dispose');
+  }
+});
+
+map.on('pointermove', function (event) {
+  if (map.hasFeatureAtPixel(event.pixel)) {
+    map.getViewport().style.cursor = 'pointer';
+  } else {
+    map.getViewport().style.cursor = 'inherit';
+  }
 });
