cce10212fae2e9a806d915295842e098348d216b
Raymond Mo <raymondmo@berkeley.com>
2021-10-31 13:18:34 -0700

    Added functionality for plotting multiple points

--- HABV/HabWebApp/src/index.js
+++ HABV/HabWebApp/src/index.js
@@ -14,12 +14,47 @@ import 'ol/ol.css';
 
 //import {getPlanetApiKey} from './login.js'
 
-function plotPoint(lon, lat) {
-  return fromLonLat([lon, lat]);
+// function plotPoint(lon, lat) {
+//   return fromLonLat([lon, lat]);
+// }
+
+
+/**
+Takes a data point and returns the location
+**/
+function getLocationFromData(dataPoint){
+  return fromLonLat([dataPoint.lon, dataPoint.lat]);
 }
 
-const point = new Point(plotPoint(-122.2730, 37.8715));
+/**
+Takes an array of data points and returns an array of Feature objects of plottable points
+**/
+function getFeaturesFromData(dataArray){
+  const res = [];
+  for(let i = 0; i < dataArray.length; i++){
+    res[i] = new Feature(new Point(getLocationFromData(dataArray[i])));
+  }
+  return res;
+}
 
+//Constants For testing
+//const point = new Point(plotPoint(-122.2730, 37.8715));
+var dataOne = {
+  lon: -122.2730,
+  lat: 37.8715
+}
+var dataTwo = {
+  lon: -122.3,
+  lat: 37.8715
+}
+var dataThree = {
+  lon: -122.35,
+  lat: 37.8715
+
+}
+const data = [dataOne, dataTwo, dataThree];
+
+//Map object that is plotted
 const map = new Map({
  target: 'map',
  layers: [
@@ -30,7 +65,7 @@ const map = new Map({
    }),
    new VectorLayer({
     source: new VectorSource({
-      features: [new Feature(point)],
+      features: getFeaturesFromData(data),
     }),
     style: new Style({
       image: new Circle({
